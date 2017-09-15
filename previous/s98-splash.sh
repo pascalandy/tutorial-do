94794c9e3815fdb0d172c7de7614299315da3f78
Pascal Andy <pascal@firepress.org>
2017-09-15 09:35:19 -0400

    add previous work 2

--- previous/s98-splash.sh
+++ previous/s98-splash.sh
@@ -11,15 +11,5 @@ set -o pipefail
 # Turn on traces, useful while debugging but commented out by default
 # set -o xtrace
 
-### Show this which script is running
-echo "checkpoint 980 $(date +%Y-%m-%d_%Hh%Mm%S)" >> ~/temp/provisionninglogs.txt && echo;
-echo "SCRIPT s98-splash.sh"; echo; echo; sleep 2;
 
-# Copy file
-echo "checkpoint 981 $(date +%Y-%m-%d_%Hh%Mm%S)" >> ~/temp/provisionninglogs.txt && echo;
-#cp /root/temp/infra-as-code/files/splash.txt /etc/splash.txt
-#echo "Banner /etc/splash.txt" >> /etc/ssh/sshd_config
-
-# End of this script
-echo "checkpoint 982 $(date +%Y-%m-%d_%Hh%Mm%S)" >> ~/temp/provisionninglogs.txt && echo
-hostname && echo
+### STOP HERE!!
